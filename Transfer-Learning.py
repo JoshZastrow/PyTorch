8124033f011b8f9d2b50221f2ecbde84ead8591e
Josh Zastrow <j.a.zastrow.jr@gmail.com>
2017-05-14 20:56:56 -0400

    first Commit

---
+++ Transfer-Learning.py
@@ -0,0 +1,17 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt 
+
+import time
+import copy
+import os
+import sys
+
+plt.ion()
